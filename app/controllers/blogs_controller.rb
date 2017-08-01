a78b3c07d9efb8cf2aa70cdb87f11078dcfcd03f
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 16:23:56 +0000

    Refactored alerts into helper method

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -33,7 +33,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'You did it.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
       end
@@ -57,7 +57,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Blog has been deleted.' }
     end
   end
 
